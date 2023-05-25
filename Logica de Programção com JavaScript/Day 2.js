5b1602497a10b2cfad0ca099c8107d238aeee6ff
Gabs77u <gabrielaraujoseven@gmail.com>
2023-05-25 17:11:35 -0300

    Versão 1.0.1

--- Logica de Programção com JavaScript/Day 2.js
+++ Logica de Programção com JavaScript/Day 2.js
@@ -1,11 +1,11 @@
 let Nome = prompt("Qual o seu nome?:");
 let Idade = prompt("Quantos anos você tem?:");
 let Linguagem = prompt("Qual linguagem de programação você está estudando?:");
+let Condicao = prompt("Você gosta de estudar ", Linguagem, "? Responda com o número 1 para SIM ou 2 para NÃO.:");
 
 const msg = `"Olá, ${Nome}! Você tem ${Idade} anos e já esta aprendendo ${Linguagem}"`;
 alert(msg);
 
-let Condicao = prompt("Você gosta de estudar ", Linguagem, "? Responda com o número 1 para SIM ou 2 para NÃO.:");
 
 if(Condicao == 1){
 alert("Muito bom! Continue estudando e você terá muito sucesso !!!")
