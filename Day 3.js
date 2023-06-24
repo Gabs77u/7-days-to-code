d87d01e6ab48677d54b564c0344ce4aa77938987
Gabs77u <gabrielaraujoseven@gmail.com>
2023-06-24 17:11:35 -0300

    Beta 0.1

--- Day 3.js
+++ Day 3.js
@@ -1,10 +1,14 @@
 const nome = prompt ("Me diga seu nome, por favor:");
 alert (`Olá,${nome}`);
 //Interação de Usuario 1
-const FrontendOrBackend = prompt(`${nome} ,deseja seguir para a area de Front-End ou Back-End ?`);
+const interacao = prompt(`${nome} ,deseja seguir para a area de Front-End ou Back-End ?`);
 alert (`Certo`)
-if (FrontendOrBackend === "Front-End"||"Frontend"||"frontend"||"front-end"){
-    const ReactOrVue = prompt("Deseja seguir para a area de React ou Vue ?");
-    if(ReactOrVue === "React"||"react"||"REACT"){
-    }
+if (interacao === "Front-End"||"Frontend"||"frontend"||"front-end"){
+    FrontendOrBackend = prompt("Deseja seguir para a area de React ou Vue ?");
+}else if (interacao === "Back-End"||"back-end"||"backend"||"Backend"){
+    FrontendOrBackend = prompt("Você quer aprender C# ou Java?");
 }
+else{
+    alert("Insira uma opção valida !!!");
+return;
+};
